ebac1e109bd915a41e8cae0d54d386140aba2586
EpicKiwi <lelycandu76@gmail.com>
2014-10-22 17:30:17 +0200

    Unification des deux fonction de catégorie

--- src/BeTeen/ForumBundle/Controller/ForumController.php
+++ src/BeTeen/ForumBundle/Controller/ForumController.php
@@ -6,19 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ForumController extends Controller
 {
-	public function indexAction()
-	{
-		$manager = $this->getDoctrine()->getManager();
-		$repository = $manager->getRepository("BeTeenForumBundle:Categorie");
-		$position = $repository->findOneBy(array("nom"=>"Index"));
-		$categories = $repository->findAllSubcategories($position);
-        return $this->render('BeTeenForumBundle:Forum:index.html.twig',array("listeCategories"=>$categories,"categorieActuelle"=>$position));
-	}
     public function categorieAction($categorie)
     {
 		$manager = $this->getDoctrine()->getManager();
 		$repository = $manager->getRepository("BeTeenForumBundle:Categorie");
-		$position = $repository->find($categorie);
+		if($categorie == 0)
+		{
+			$position = $repository->findOneBy(array("nom"=>"Index"));
+		}
+		else
+		{
+			$position = $repository->find($categorie);
+		}
 		$categories = $repository->findAllSubcategories($position);
         return $this->render('BeTeenForumBundle:Forum:index.html.twig',array("listeCategories"=>$categories,"categorieActuelle"=>$position));
     }
