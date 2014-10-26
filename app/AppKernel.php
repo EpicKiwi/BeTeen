c0b930fd3b6ff55bad7aeabf8490bca9db3dc016
Baptiste <lelycandu76@gmail.com>
2014-10-26 21:57:13 +0100

    Clarification légère

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new BeTeen\ForumBundle\BeTeenForumBundle(),
             new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+            new BeTeen\ForumBundle\BeTeenForumBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
