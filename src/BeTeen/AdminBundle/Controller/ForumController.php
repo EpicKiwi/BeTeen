da5ad1f42c88cb3d9281f4f836daac753e14fc9c
Baptiste <lelycandu76@gmail.com>
2014-10-30 12:19:53 +0100

    Suppression des sujet possible

--- src/BeTeen/AdminBundle/Controller/ForumController.php
+++ src/BeTeen/AdminBundle/Controller/ForumController.php
@@ -38,4 +38,30 @@ class ForumController extends Controller
             return $this->render('BeTeenAdminBundle:Forum:sujets.html.twig',array("sujets"=>$sujets));
         }
     }
+    
+    public function supprimerSujetAction($sujet)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository("BeTeenForumBundle:SujetStandard");
+        $sujetSupprime = $repository->findOneBySlug($sujet);
+        if($sujet == null)
+        {
+            throw $this->createNotFoundException("Ce sujet n'existe pas");
+        }
+        
+        $message = "Voulez vous vraiment supprimer le sujet ".$sujetSupprime->getTitre()." ?";
+        
+        $form = $this->createFormBuilder($sujetSupprime)->getForm();
+        
+        $requete = $this->get("request");
+        if($requete->getMethod() == "POST")
+        {
+            $this->get('session')->getFlashBag()->add('info','Le sujet '.$sujetSupprime->getTitre()." a été supprimer !");
+            $manager->remove($sujetSupprime);
+            $manager->flush();
+            return $this->redirect($this->generateUrl("be_teen_admin_forum_sujets"));
+        }
+        
+        return $this->render('BeTeenAdminBundle:Forum:supprimer.html.twig',array("form"=>$form->createView(),"message"=>$message));
+    }
 }
