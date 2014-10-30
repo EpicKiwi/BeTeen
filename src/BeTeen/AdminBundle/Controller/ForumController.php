e747b988d27f3b3d2b85a9ceb793c219fd87f350
Baptiste <lelycandu76@gmail.com>
2014-10-30 14:07:58 +0100

    Suppression de catégorie desormet possible

--- src/BeTeen/AdminBundle/Controller/ForumController.php
+++ src/BeTeen/AdminBundle/Controller/ForumController.php
@@ -44,7 +44,7 @@ class ForumController extends Controller
         $manager = $this->getDoctrine()->getManager();
         $repository = $manager->getRepository("BeTeenForumBundle:SujetStandard");
         $sujetSupprime = $repository->findOneBySlug($sujet);
-        if($sujet == null)
+        if($sujetSupprime == null)
         {
             throw $this->createNotFoundException("Ce sujet n'existe pas");
         }
@@ -56,10 +56,83 @@ class ForumController extends Controller
         $requete = $this->get("request");
         if($requete->getMethod() == "POST")
         {
-            $this->get('session')->getFlashBag()->add('info','Le sujet '.$sujetSupprime->getTitre()." a été supprimer !");
+            $this->get('session')->getFlashBag()->add('info','Le sujet '.$sujetSupprime->getTitre()." a été supprimé !");
             $manager->remove($sujetSupprime);
             $manager->flush();
             return $this->redirect($this->generateUrl("be_teen_admin_forum_sujets"));
+        }
+        
+        return $this->render('BeTeenAdminBundle:Forum:supprimer.html.twig',array("form"=>$form->createView(),"message"=>$message));
+    }
+    
+    public function categoriesAction($categorie)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository("BeTeenForumBundle:Categorie");
+        $position = $repository->findOneBySlug($categorie);
+        if($position == null)
+        {
+            throw $this->createNotFoundException("Cette catégorie n'existe pas");
+        }
+        
+        $categories = $repository->children($position,true);
+        
+        return $this->render("BeTeenAdminBundle:Forum:categories.html.twig",array("position"=>$position, "categories"=>$categories));
+    }
+    
+    public function monterCategorieAction($categorie)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository("BeTeenForumBundle:Categorie");
+        $position = $repository->findOneBySlug($categorie);
+        if($position == null)
+        {
+            throw $this->createNotFoundException("Cette catégorie n'existe pas");
+        }
+        
+        $repository->moveUp($position,1);
+        $this->get('session')->getFlashBag()->add('info','La catégorie '.$position->getNom()." a été décalé d'un rang vers le haut");
+                
+        return $this->redirect($this->generateUrl("be_teen_admin_forum_categories",array("categorie"=>$position->getParent()->getSlug())));
+    }
+    
+    public function descendreCategorieAction($categorie)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository("BeTeenForumBundle:Categorie");
+        $position = $repository->findOneBySlug($categorie);
+        if($position == null)
+        {
+            throw $this->createNotFoundException("Cette catégorie n'existe pas");
+        }
+        
+        $repository->moveDown($position,1);
+        $this->get('session')->getFlashBag()->add('info','La catégorie '.$position->getNom()." a été décalé d'un rang vers le bas");
+                
+        return $this->redirect($this->generateUrl("be_teen_admin_forum_categories",array("categorie"=>$position->getParent()->getSlug())));
+    }
+    
+    public function supprimerCategorieAction($categorie)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository("BeTeenForumBundle:Categorie");
+        $cat = $repository->findOneBySlug($categorie);
+        if($cat == null)
+        {
+            throw $this->createNotFoundException("Ce sujet n'existe pas");
+        }
+        
+        $message = "Voulez vous vraiment supprimer la Catégorie ".$cat->getNom()." ?<br/>Cela entrainera la suppression de toutes les catégories parentes et de tout les sujets attachés a ces catégories";
+        
+        $form = $this->createFormBuilder($cat)->getForm();
+        
+        $requete = $this->get("request");
+        if($requete->getMethod() == "POST")
+        {
+            $this->get('session')->getFlashBag()->add('info','La catégorie '.$cat->getNom()." a été supprimée !");
+            $manager->remove($cat);
+            $manager->flush();
+            return $this->redirect($this->generateUrl("be_teen_admin_forum_categories",array("categorie"=>$cat->getParent()->getSlug())));
         }
         
         return $this->render('BeTeenAdminBundle:Forum:supprimer.html.twig',array("form"=>$form->createView(),"message"=>$message));
