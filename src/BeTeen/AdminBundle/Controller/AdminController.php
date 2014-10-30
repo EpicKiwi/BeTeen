fef4249e1edd3eaa79e16dddf920be6954589fd6
Baptiste <lelycandu76@gmail.com>
2014-10-30 11:42:49 +0100

    Gestion du flashbag sur l'administration

--- src/BeTeen/AdminBundle/Controller/AdminController.php
+++ src/BeTeen/AdminBundle/Controller/AdminController.php
@@ -8,6 +8,8 @@ class AdminController extends Controller
 {
     public function indexAction()
     {
+        $this->get('session')->getFlashBag()->add('info','Bienvenue sur l\'administration du site');
+        
         $manager = $this->getDoctrine()->getManager();
         $repository = $manager->getRepository("BeTeenForumBundle:SujetStandard");
         $sujets = $repository->findAllLimitorderByDate(10);
