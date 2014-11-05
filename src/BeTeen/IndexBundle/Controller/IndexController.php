b8b6e3b86727b7337fb98b920de30b96c1998d99
Baptiste <lelycandu76@gmail.com>
2014-11-05 17:12:02 +0100

    Resolution de bug controller

--- src/BeTeen/IndexBundle/Controller/IndexController.php
+++ src/BeTeen/IndexBundle/Controller/IndexController.php
@@ -4,7 +4,7 @@ namespace BeTeen\IndexBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class IndexController extends Controller
 {
     public function indexAction()
     {
