16ac40dfd96b17eafd4c39dd66fff3055906ca3d
Baptiste <lelycandu76@gmail.com>
2014-11-12 21:22:27 +0100

    Admin : Ajout du formulaire d'ajout d'utilisateur
    Les utilisateurs ne peuvent etre ajoutés qu'en tant qu'utilisateur
    standard.

--- src/BeTeen/AdminBundle/Controller/UserController.php
+++ src/BeTeen/AdminBundle/Controller/UserController.php
@@ -3,6 +3,8 @@
 namespace BeTeen\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \BeTeen\UserBundle\Entity\User;
+use BeTeen\UserBundle\Form\UserType;
 
 class UserController extends Controller
 {
@@ -12,5 +14,30 @@ class UserController extends Controller
         $repository = $manager->getRepository("BeTeenUserBundle:User");
         $users = $repository->findLimit(10);
         return $this->render('BeTeenAdminBundle:User:index.html.twig',array("utilisateurs"=>$users));
+    }
+    
+    public function ajoutAction()
+    {
+        $utilisateur = new User();
+        $utilisateur->setRoles(array("ROLE_USER"));
+        $utilisateur->setSalt("");
+        
+        $form = $this->createForm(new UserType(),$utilisateur);
+        
+        $requete = $this->get("request");
+        if($requete->getMethod() == "POST")
+        {
+            $form->bind($requete);
+            if($form->isValid())
+            {
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($utilisateur);
+                $manager->flush();
+                $this->get('session')->getFlashBag()->add('info','L\'utilisateur '.$utilisateur->getUsername()." a été ajouté");
+                return $this->redirect($this->generateUrl("be_teen_admin_user"));
+            }
+        }
+        
+        return $this->render("BeTeenAdminBundle:Forum:Ajouter.html.twig",array("form"=>$form->createView()));
     }
 }
