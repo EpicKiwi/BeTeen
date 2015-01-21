5c76f29031bd0798f429b6be8f84405e958f0d10
Baptiste <lelycandu76@gmail.com>
2015-01-21 21:41:38 +0100

    reduction de la taille des images dans les footers
    Les images sont coupés et sont pres de 10x moins lourdes ... cool pour
    le chargement rapide et sur les mobiles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new BeTeen\AdminBundle\BeTeenAdminBundle(),
             new BeTeen\IndexBundle\BeTeenIndexBundle(),
             new BeTeen\UserBundle\BeTeenUserBundle(),
+            new Gregwar\ImageBundle\GregwarImageBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
