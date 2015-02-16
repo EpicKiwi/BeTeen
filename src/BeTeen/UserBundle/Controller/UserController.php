6de11fb19157c4c0239dfaf8bfffaeac823c194e
Baptiste <lelycandu76@gmail.com>
2015-02-16 20:33:30 +0100

    Soumission fonctionnel

--- src/BeTeen/UserBundle/Controller/UserController.php
+++ src/BeTeen/UserBundle/Controller/UserController.php
@@ -2,8 +2,10 @@
 
 namespace BeTeen\UserBundle\Controller;
 
+use BeTeen\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
+use BeTeen\UserBundle\Form\UserRegisterType;
 
 class UserController extends Controller
 {
@@ -36,5 +38,37 @@ class UserController extends Controller
     {
         $user = $this->getDoctrine()->getRepository("BeTeenUserBundle:User")->findOneByUsername($usr);
         return $this->render("BeTeenUserBundle:User:profil.html.twig",array("utilisateur"=>$user));
+    }
+
+    public function registerAction()
+    {
+        $user = new User();
+        $user->setRoles(array("ROLE_USER"));
+        $user->setAvatar("");
+        $user->setBackground("");
+        $user->setDescription("");
+        $form = $this->createForm(new UserRegisterType, $user);
+
+        $request = $this->get("request");
+
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            //Generation du sel
+            $user->setSalt(rand(0,pow(10,9)));
+            //Generation du mot de passe
+            $factory = $this->get('security.encoder_factory');
+            $encoder = $factory->getEncoder($user);
+            $user->setPassword($encoder->encodePassword($user->getPassword(),$user->getSalt()));
+            //Enregistrement en BDD
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+            $this->get("session")->getFlashBag()->add("info","votre compte à été crée, vous pouvez des à présent vous connecter");
+            return $this->redirect($this->generateUrl("be_teen_index_index"));
+        }
+
+        return $this->render("BeTeenUserBundle:User:enregistrement.html.twig",array("form"=>$form->createView()));
     }
 }
---
+++ src/BeTeen/UserBundle/Form/UserRegisterType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace BeTeen\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserRegisterType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username',   'text')
+            ->add('password',   'repeated',array(
+                                                'type' => 'password',
+                                                'invalid_message' => 'Les mot de passe doivent correspondre',
+                                                'options' => array('required' => true),
+                                                'first_options' => array('label' => 'Mot de passe'),
+                                                'second_options' => array('label' => 'Comfirmez votre mot de passe')
+                                                ))
+            ->add('email',      'repeated',array(
+                                                'type' => 'text',
+                                                'invalid_message' => 'Les adresses emails doivent correspondre',
+                                                'options' => array('required' => true),
+                                                'first_options' => array('label' => 'Adresse email'),
+                                                'second_options' => array('label' => 'Comfirmez votre adresse')
+                                                ));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BeTeen\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'beteen_userbundle_user';
+    }
+}
